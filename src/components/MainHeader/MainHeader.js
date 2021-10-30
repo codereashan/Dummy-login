6c4d0cc7087ce6a181d7cf1c2a94532761f8e075
Eashan Dessai <58380472+codereashan@users.noreply.github.com>
2021-10-31 00:41:49 +0530

    Update MainHeader.js

--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -7,7 +7,6 @@ const MainHeader = (props) => {
   return (
     <header className={classes["main-header"]}>
       <h1 style={{ textAlign: "center" }}>Login Page</h1>
-      {/* <Navigation isLoggedIn={props.isAuthenticated} onLogout={props.onLogout} /> */}
       <Navigation />
     </header>
   );
